69a5fe06b553e2cd05e9f18e1c4db7b66ffbde10
jcmunav63 <jcmunav63@gmail.com>
2023-08-02 08:56:02 -0600

    Finish setting up the repo.

--- src/stringlength.js
+++
@@ -1,4 +0,0 @@
-// Function to determine a STRING'S LENGTH
-function stringLength(string) {
-  return string.split('').reduce((length) => length + 1, 0);
-}
--- src/stringlength.test.js
+++
@@ -1,7 +0,0 @@
-// UNIT TEST - Function STRINGLENGTH
-// arrange
-const str = "Another one bites the dust";
-// act
-stringLength(str);
-// assert
-// mock.Received.SomeMethod();
